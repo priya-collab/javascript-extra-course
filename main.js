f16a0509c346a0ba5692ce7a16f82699b9c7c7d6
priya-collab <priyachandran3@gmail.com>
2021-01-11 19:47:59 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+console.error("this is an error");
+console.warn("this is a warning");
+const name = "priya";
+const age = 29;
+const rating =4.5;
+const y = null;
+const isGood = true;
+const x = undefined;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof x);
+
